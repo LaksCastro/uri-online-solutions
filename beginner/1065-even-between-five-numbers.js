960daf517148230086804559f2130eb9688474ca
Laks Castro <lakhsmycastro336@gmail.com>
2020-03-13 00:28:33 -0400

    Update 1065-even-between-five-numbers.js

--- beginner/1065-even-between-five-numbers.js
+++ beginner/1065-even-between-five-numbers.js
@@ -2,8 +2,10 @@ const input = require('fs').readFileSync('/dev/stdin', 'utf8');
 const lines = input.split('\n');
 
 let evenCount = 0;
+
 for (let i = 0; i < 5; i++) {
   const num = Number(lines.shift());
   if (num % 2 === 0) evenCount++;
 }
+
 console.log(evenCount + " valores pares");
