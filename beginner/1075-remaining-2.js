d8354fcb89f93f25904dd0b7b72416734b0bee2a
Laks Castro <lakhsmycastro336@gmail.com>
2020-03-13 08:46:53 -0400

    Create 1075-remaining-2.js

---
+++ beginner/1075-remaining-2.js
@@ -0,0 +1,8 @@
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const lines = input.split('\n');
+
+const num = Number(lines.shift());
+
+for (let i = 0; i < 10000; i++) {
+  if (i % num === 2) console.log(i);
+}
