4bfc274606abe48bd28d98ceeaafd3f01850a90e
Laks Castro <lakhsmycastro336@gmail.com>
2020-07-19 01:46:45 -0300

    Update 1011-sphere.js

--- javascript/beginner/1011-sphere.js
+++ javascript/beginner/1011-sphere.js
@@ -2,8 +2,7 @@ const input = require('fs').readFileSync('/dev/stdin', 'utf8');
 const lines = input.split('\n');
 
 const pi = 3.14159;
-const raio = Number(lines.shift());
-const volume = (4 / 3) * pi * raio ** 3;
+const r = Number(lines.shift());
+const v = (4 / 3) * pi * r ** 3;
 
-const mensagem = `VOLUME = ${volume.toFixed(3)}`;
-console.log(mensagem);
+console.log(`VOLUME = ${v.toFixed(3)}`);
