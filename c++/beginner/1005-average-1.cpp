09fad7ebd7ed103ebcf8f468add2a6960e590345
Laks Castro <lakhsmycastro336@gmail.com>
2020-07-26 14:15:05 -0300

    Update 1005-average-1.cpp

--- c++/beginner/1005-average-1.cpp
+++ c++/beginner/1005-average-1.cpp
@@ -4,22 +4,22 @@
 using namespace std;
  
 int main() {
-    double n1, n2;
+  double n1, n2;
     
-    cout << fixed << setprecision(5);
+  cout << fixed << setprecision(5);
     
-    cin >> n1;
-    cin >> n2;
+  cin >> n1;
+  cin >> n2;
     
-    double p1 = 3.5;
-    double p2 = 7.5;
+  double p1 = 3.5;
+  double p2 = 7.5;
     
-    n1 *= p1;
-    n2 *= p2;
+  n1 *= p1;
+  n2 *= p2;
     
-    double m = (n1 + n2) / (p1 + p2);
+  double m = (n1 + n2) / (p1 + p2);
     
-    cout << "MEDIA = " << m << endl;
+  cout << "MEDIA = " << m << endl;
     
-    return 0;
+  return 0;
 }
