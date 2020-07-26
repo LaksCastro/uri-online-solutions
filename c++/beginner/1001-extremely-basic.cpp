e0b0da683c5b7e2220908ddec437d8a625bb26c8
Laks Castro <lakhsmycastro336@gmail.com>
2020-07-26 14:15:23 -0300

    Update 1001-extremely-basic.cpp

--- c++/beginner/1001-extremely-basic.cpp
+++ c++/beginner/1001-extremely-basic.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 
 int main() {
-    int n1, n2, sum;
+  int n1, n2, sum;
     
-    cin >> n1;
-    cin >> n2;
+  cin >> n1;
+  cin >> n2;
     
-    sum = n1 + n2;
+  sum = n1 + n2;
   
-    cout << "X = " << sum << endl;
+  cout << "X = " << sum << endl;
  
-    return 0;
+  return 0;
 }
