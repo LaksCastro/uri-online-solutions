f9ce2b79b5fcf3f44feb1c74a0368347e4370303
Laks Castro <lakhsmycastro336@gmail.com>
2020-08-04 23:12:58 -0300

    Update 1013-the-greatest.cpp

--- c++/beginner/1013-the-greatest.cpp
+++ c++/beginner/1013-the-greatest.cpp
@@ -3,19 +3,18 @@
 
 using namespace std;
 
-int main()
-{
-    int s[3], max;
-    
-    cin >> s[0] >> s[1] >> s[2];
-    
-    for(int i = 0; i < 3; i++) {
-        if(s[i] > max) {
-            max = s[i];
-        }
+int main() {
+  int s[3], max;
+
+  cin >> s[0] >> s[1] >> s[2];
+
+  for (int i = 0; i < 3; i++) {
+    if (s[i] > max) {
+      max = s[i];
     }
-    
-    cout << max << " eh o maior" << endl;
-    
-    return 0;
+  }
+
+  cout << max << " eh o maior" << endl;
+
+  return 0;
 }
