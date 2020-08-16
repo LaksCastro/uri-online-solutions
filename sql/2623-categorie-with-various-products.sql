f3444fb85ad1fc6c92070246de7e20130134850f
Laks Castro <lakhsmycastro336@gmail.com>
2020-08-16 10:56:36 -0300

    Update 2623-categorie-with-various-products.sql

--- sql/2623-categorie-with-various-products.sql
+++ sql/2623-categorie-with-various-products.sql
@@ -1,5 +1,11 @@
-select products.name, categories.name from products 
-join categories on categories.id = products.id_categories
-where products.amount > 100 and 
-categories.id in(1, 2, 3, 6, 9)
-order by categories.id asc;
+SELECT products.name,
+       categories.name
+FROM products
+JOIN categories ON categories.id = products.id_categories
+WHERE products.amount > 100
+  AND categories.id IN(1,
+                       2,
+                       3,
+                       6,
+                       9)
+ORDER BY categories.id ASC;
