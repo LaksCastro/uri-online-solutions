02b257d61484f8738d6d55497dde647562f19482
Laks Castro <lakhsmycastro336@gmail.com>
2020-08-16 10:57:04 -0300

    Update 2625-cpf-validation.sql

--- sql/2625-cpf-validation.sql
+++ sql/2625-cpf-validation.sql
@@ -1,5 +1,3 @@
-select 
-substring(cpf, 1, 3) || '.' || 
-substring(cpf, 4, 3) || '.' || 
-substring(cpf, 7, 3) || '-' || substring(cpf, 10, 2)
-from natural_person;
+SELECT substring(cpf, 1, 3) || '.' || substring(cpf, 4, 3) || '.' || substring(cpf, 7, 3) || '-' || substring(cpf, 10, 2)
+FROM natural_person;
+
