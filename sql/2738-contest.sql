a34ace1e395925f73a02a0cfb32b13f2766bb38b
Laks Castro <lakhsmycastro336@gmail.com>
2020-08-16 10:58:43 -0300

    Update 2738-contest.sql

--- sql/2738-contest.sql
+++ sql/2738-contest.sql
@@ -1,4 +1,5 @@
-select candidate.name, cast((((score.math * 2) + (score.specific * 3) + (score.project_plan * 5)) / 10) as numeric(15, 2)) as avg
-from candidate
-join score on score.candidate_id = candidate.id
-order by avg desc;
+SELECT candidate.name,
+       cast((((score.math * 2) + (score.specific * 3) + (score.project_plan * 5)) / 10) AS numeric(15, 2)) AS AVG
+FROM candidate
+JOIN score ON score.candidate_id = candidate.id
+ORDER BY AVG DESC;
