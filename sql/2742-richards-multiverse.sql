feffb0a2b4768237e9ae71d246e402d33c00dfcd
Laks Castro <lakhsmycastro336@gmail.com>
2020-08-16 10:59:25 -0300

    Update 2742-richards-multiverse.sql

--- sql/2742-richards-multiverse.sql
+++ sql/2742-richards-multiverse.sql
@@ -1,4 +1,8 @@
-select life_registry.name, round((life_registry.omega * 1.618), 3) as "The N Factor" from life_registry
-join dimensions on dimensions.id = life_registry.dimensions_id
-where dimensions.name in('C774', 'C875') and life_registry.name like 'Richard%'
-order by life_registry.omega asc;
+SELECT life_registry.name,
+       round((life_registry.omega * 1.618), 3) AS "The N Factor"
+FROM life_registry
+JOIN dimensions ON dimensions.id = life_registry.dimensions_id
+WHERE dimensions.name IN('C774',
+                         'C875')
+  AND life_registry.name LIKE 'Richard%'
+ORDER BY life_registry.omega ASC;
