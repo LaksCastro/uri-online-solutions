98ff3e8358d1a46848407197729d3d03a51dbe52
Laks Castro <lakhsmycastro336@gmail.com>
2020-08-16 10:55:30 -0300

    Update 2618-imported-products.sql

--- sql/2618-imported-products.sql
+++ sql/2618-imported-products.sql
@@ -1,4 +1,8 @@
-select products.name, providers.name, categories.name from categories
-join products on products.id_categories = categories.id
-join providers on products.id_providers = providers.id
-where providers.name = 'Sansul SA' and categories.name = 'Imported';
+SELECT products.name,
+       providers.name,
+       categories.name
+FROM categories
+JOIN products ON products.id_categories = categories.id
+JOIN providers ON products.id_providers = providers.id
+WHERE providers.name = 'Sansul SA'
+  AND categories.name = 'Imported';
