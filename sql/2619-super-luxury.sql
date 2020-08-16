3197cc89657efb4d702314d590667922a480694b
Laks Castro <lakhsmycastro336@gmail.com>
2020-08-16 10:55:50 -0300

    Update 2619-super-luxury.sql

--- sql/2619-super-luxury.sql
+++ sql/2619-super-luxury.sql
@@ -1,4 +1,8 @@
-select products.name, providers.name, products.price from products 
-join providers on providers.id = products.id_providers
-join categories on categories.id = products.id_categories
-where products.price > 1000 and categories.name = 'Super Luxury';
+SELECT products.name,
+       providers.name,
+       products.price
+FROM products
+JOIN providers ON providers.id = products.id_providers
+JOIN categories ON categories.id = products.id_categories
+WHERE products.price > 1000
+  AND categories.name = 'Super Luxury';
