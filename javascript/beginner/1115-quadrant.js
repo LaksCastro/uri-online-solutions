a5241073b59483ef9a4b29e99d44fdea191729d2
LaksCastro <lakhsmycastro336@gmail.com>
2020-08-29 00:52:33 +0100

    Update 1115-quadrant.js

--- javascript/beginner/1115-quadrant.js
+++ javascript/beginner/1115-quadrant.js
@@ -5,18 +5,20 @@ let getout = false;
 
 while (!getout) {
   const string = lines.shift();
+  
   const [x, y] = string.split(" ").map(item => Number(item));
+  
   if (x === 0 || y === 0) {
     getout = true;
     break;
   } else {
-    if(x > 0 && y > 0){
+    if(x > 0 && y > 0) {
       console.log("primeiro");
-    }else if(x > 0 && y < 0){
+    } else if(x > 0 && y < 0) {
       console.log("quarto");
-    }else if(x < 0 && y > 0){
+    } else if(x < 0 && y > 0) {
       console.log("segundo");
-    }else if(x < 0 && y < 0){
+    } else if(x < 0 && y < 0) {
       console.log("terceiro");
     }
   }
