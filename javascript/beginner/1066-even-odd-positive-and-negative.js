52955a7d2964f840ff68ab7352cc47344dc803ba
LaksCastro <lakhsmycastro336@gmail.com>
2020-09-13 10:47:40 -0300

    Update 1066-even-odd-positive-and-negative.js

--- javascript/beginner/1066-even-odd-positive-and-negative.js
+++ javascript/beginner/1066-even-odd-positive-and-negative.js
@@ -3,20 +3,28 @@ const lines = input.split('\n');
 
 let positives = 0;
 let negatives = 0;
+
 let odd = 0;
 let even = 0;
 
 for (let i = 0; i < 5; i++) {
   const num = Number(lines.shift());
 
-  if (num % 2 === 0) even++;
-  else odd++;
+  if (num % 2 === 0) {
+    even++;
+  } else {
+     odd++;
+  }
 
-  if (num > 0) positives++;
-  else if (num < 0) negatives++;
+  if (num > 0) {
+    positives++;
+  } else if (num < 0) {
+    negatives++;
+  }
 }
 
 console.log(even + " valor(es) par(es)");
 console.log(odd + " valor(es) impar(es)");
+
 console.log(positives + " valor(es) positivo(s)");
 console.log(negatives + " valor(es) negativo(s)");
