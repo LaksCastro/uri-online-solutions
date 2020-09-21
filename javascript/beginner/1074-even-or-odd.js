58a85b6b521183d2c7fb85792e1f1715ae3dde0e
LaksCastro <lakhsmycastro336@gmail.com>
2020-09-21 00:26:00 -0300

    Update 1074-even-or-odd.js

--- javascript/beginner/1074-even-or-odd.js
+++ javascript/beginner/1074-even-or-odd.js
@@ -3,13 +3,15 @@ const lines = input.split('\n');
 
 const casesLength = Number(lines.shift());
 
-for(let caseIndex = 0; caseIndex < casesLength; caseIndex++){
+for (let caseIndex = 0; caseIndex < casesLength; caseIndex++) {
     const num = Number(lines.shift());
-    if(num === 0){
+    
+    if (num === 0) {
         console.log("NULL");
-    }else{
+    } else {
         const signal = num > 0 ? "POSITIVE" : "NEGATIVE";
         const even = num % 2 === 0 ? "EVEN" : "ODD";
+        
         console.log(even + " " + signal);
     }
 }
