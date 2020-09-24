1b289b555ea09e025dcdc6ade9b8a05a9c1339e3
LaksCastro <lakhsmycastro336@gmail.com>
2020-09-24 12:14:48 -0300

    Update 1064-positives-and-average.js

--- javascript/beginner/1064-positives-and-average.js
+++ javascript/beginner/1064-positives-and-average.js
@@ -5,11 +5,12 @@ let positives = [];
 let sum = 0;
 
 for(let i = 0; i < 6; i++){
-	const num = Number(lines.shift());
-	if(num >= 0){
-		positives.push(num);
-		sum += num;
-	}
+  const num = Number(lines.shift());
+	
+  if(num >= 0){
+    positives.push(num);
+    sum += num;
+  }
 }
 
 const average = sum / positives.length;
