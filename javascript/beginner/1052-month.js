84716d8a40080c284598aaf63b47c9a5a6e19c38
LaksCastro <lakhsmycastro336@gmail.com>
2020-09-25 19:05:24 -0300

    Update 1052-month.js

--- javascript/beginner/1052-month.js
+++ javascript/beginner/1052-month.js
@@ -4,18 +4,18 @@ const lines = input.split('\n');
 const month = lines.shift();
 
 const months = {
-    "1": "January",
-    "2": "February",
-    "3": "March",
-    "4": "April",
-    "5": "May",
-    "6": "June",
-    "7": "July",
-    "8": "August",
-    "9": "September",
-    "10": "October",
-    "11": "November",
-    "12": "December"
+  "1": "January",
+  "2": "February",
+  "3": "March",
+  "4": "April",
+  "5": "May",
+  "6": "June",
+  "7": "July",
+  "8": "August",
+  "9": "September",
+  "10": "October",
+  "11": "November",
+  "12": "December"
 };
 
 console.log(months[month]);
