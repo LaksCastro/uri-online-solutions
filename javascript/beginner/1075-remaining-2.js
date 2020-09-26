7d679ad29ea2da8651b9aee6484dc732d8a10dcb
LaksCastro <lakhsmycastro336@gmail.com>
2020-09-26 14:02:05 -0300

    Update 1075-remaining-2.js

--- javascript/beginner/1075-remaining-2.js
+++ javascript/beginner/1075-remaining-2.js
@@ -4,5 +4,7 @@ const lines = input.split('\n');
 const num = Number(lines.shift());
 
 for (let i = 0; i < 10000; i++) {
-  if (i % num === 2) console.log(i);
+  if (i % num === 2) {
+    console.log(i);
+  }
 }
