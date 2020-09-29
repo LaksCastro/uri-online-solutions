f174d2e3564fed19b44e58e4ae54351e91a41da9
LaksCastro <lakhsmycastro336@gmail.com>
2020-09-29 13:48:27 -0300

    Update 1099-sum-of-consecutive-odd-numbers-ii.js

--- javascript/beginner/1099-sum-of-consecutive-odd-numbers-ii.js
+++ javascript/beginner/1099-sum-of-consecutive-odd-numbers-ii.js
@@ -5,15 +5,20 @@ const cases = Number(lines.shift());
 
 for (let caseI = 0; caseI < cases; caseI++) {
   const string = lines.shift();
+  
   const [a, b] = string.split(" ").map(item => Number(item));
 
   if (a === b || a + 1 === b || b + 1 === a) {
     console.log("0");
   } else {
     let sum = 0;
+    
     let [numA, numB] = a > b ? [a, b + 1] : [b, a + 1];
+    
     for (; numB < numA; numB++) {
-      if (numB % 2 !== 0) sum += numB;
+      if (numB % 2 !== 0) {
+        sum += numB;
+      }
     }
     console.log(sum);
   }
