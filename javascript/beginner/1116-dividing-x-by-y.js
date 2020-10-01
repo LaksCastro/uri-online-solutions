882ae120c307d9a34a7671dbd153b0f58db5a8c0
LaksCastro <lakhsmycastro336@gmail.com>
2020-10-01 15:43:38 -0300

    Update 1116-dividing-x-by-y.js

--- javascript/beginner/1116-dividing-x-by-y.js
+++ javascript/beginner/1116-dividing-x-by-y.js
@@ -5,7 +5,12 @@ const casesLength = Number(lines.shift());
 
 for (let caseIndex = 0; caseIndex < casesLength; caseIndex++) {
   const string = lines.shift();
+  
   const [a, b] = string.split(" ").map(item => Number(item));
-  if (b === 0) console.log("divisao impossivel");
-  else console.log((a / b).toFixed(1));
+  
+  if (b === 0) {
+    console.log("divisao impossivel");
+  } else {
+    console.log((a / b).toFixed(1));
+  }
 }
