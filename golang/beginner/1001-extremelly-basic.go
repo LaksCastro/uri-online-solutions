42180adcd8737b9d73cade0d454f22b50b5b7fc2
LaksCastro <lakhsmycastro336@gmail.com>
2020-10-03 11:48:30 -0300

    Update 1001-extremelly-basic.go

--- golang/beginner/1001-extremelly-basic.go
+++ golang/beginner/1001-extremelly-basic.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
+  "fmt"
+  "strconv"
 )
 
 func main() {
-	var n1 int
-	var n2 int
+  var n1 int
+  var n2 int
 
-	fmt.Scanln(&n1)
-	fmt.Scanln(&n2)
+  fmt.Scanln(&n1)
+  fmt.Scanln(&n2)
 
-	var sum = n1 + n2
+  var sum = n1 + n2
 
-	fmt.Println("X = " + strconv.Itoa(sum))
+  fmt.Println("X = " + strconv.Itoa(sum))
 }
