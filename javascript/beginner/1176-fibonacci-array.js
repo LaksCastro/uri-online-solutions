14889daeae8dd62b8723a23491902d2c32abbd39
LaksCastro <lakhsmycastro336@gmail.com>
2020-10-06 12:34:39 -0300

    Update 1176-fibonacci-array.js

--- javascript/beginner/1176-fibonacci-array.js
+++ javascript/beginner/1176-fibonacci-array.js
@@ -4,12 +4,18 @@ const lines = input.split('\n');
 const cases = Number(lines.shift());
 
 const fib = n => {
-  if (n === 0) return [0];
+  if (n === 0) {
+    return [0];
+  }
 
-  if (n === 1) return [0, 1];
+  if (n === 1) {
+    return [0, 1];
+  }
 
   const s = fib(n - 1);
+  
   s.push(s[s.length - 1] + s[s.length - 2]);
+  
   return s;
 };
 
