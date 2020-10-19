947f7258c57bb9124455de2941cb7d81b6e4e5a1
LaksCastro <lakhsmycastro336@gmail.com>
2020-10-19 07:41:43 -0300

    Update 1041-coordinates-of-a-point.js

--- javascript/beginner/1041-coordinates-of-a-point.js
+++ javascript/beginner/1041-coordinates-of-a-point.js
@@ -4,21 +4,21 @@ const lines = input.split('\n');
 const [x, y] = lines.shift().split(" ").map(item => Number(item));
 
 if (!x && !y) {
-	console.log("Origem");
+  console.log("Origem");
 } else {
-	if(!x) {
-		console.log("Eixo Y");
-	} else if(!y) {
-		console.log("Eixo X");
-	} else {
-		if (x > 0 && y > 0) {
-			console.log("Q1");
-		} else if (x > 0 && y < 0){
-			console.log("Q4")
-		} else if (x < 0 && y > 0){
-			console.log("Q2")
-		} else if (x < 0 && y < 0){
-			console.log("Q3")
-		}
-	}
+  if(!x) {
+    console.log("Eixo Y");
+  } else if(!y) {
+    console.log("Eixo X");
+  } else {
+    if (x > 0 && y > 0) {
+      console.log("Q1");
+    } else if (x > 0 && y < 0){
+      console.log("Q4")
+    } else if (x < 0 && y > 0){
+      console.log("Q2")
+    } else if (x < 0 && y < 0){
+      console.log("Q3")
+    }
+  }
 }
