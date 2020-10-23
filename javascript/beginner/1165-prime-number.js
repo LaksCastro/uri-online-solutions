0a0de82e6b7c70d0e1694c5f4bf10b732b265f28
LaksCastro <lakhsmycastro336@gmail.com>
2020-10-23 09:19:59 -0300

    Update 1165-prime-number.js

--- javascript/beginner/1165-prime-number.js
+++ javascript/beginner/1165-prime-number.js
@@ -4,21 +4,21 @@ const lines = input.split('\n');
 const cases = Number(lines.shift());
 
 for (let x = 0; x < cases; x++) {
-    const num = Number(lines.shift());
+  const num = Number(lines.shift());
 
-    let divisors = 0;
+  let divisors = 0;
 
-    for (let a = 1; a <= num; a++) {
-        if (num % a === 0) {
-          divisors++;
-        }
-        
-        if (divisors > 2) {
-          break;
-        }
+  for (let a = 1; a <= num; a++) {
+    if (num % a === 0) {
+      divisors++;
     }
+        
+    if (divisors > 2) {
+      break;
+    }
+  }
 
-    const message = num + (divisors === 2 ? " eh primo" : " nao eh primo");
+  const message = num + (divisors === 2 ? " eh primo" : " nao eh primo");
 
-    console.log(message);
+  console.log(message);
 }
