f201c8848c3a4b422504a41a00e5b022e2a17cb0
LaksCastro <lakhsmycastro336@gmail.com>
2020-10-28 00:06:31 -0300

    Update 1080-highest-and-position.js

--- javascript/beginner/1080-highest-and-position.js
+++ javascript/beginner/1080-highest-and-position.js
@@ -4,9 +4,9 @@ const lines = input.split('\n');
 let maior = 0;
 let index = 0;
 
-for(let i = 0; i < 100; i++){
+for (let i = 0; i < 100; i++) {
     const num = Number(lines.shift());
-    if(num > maior) {
+    if (num > maior) {
         maior = num;
         index = i + 1;
     }
