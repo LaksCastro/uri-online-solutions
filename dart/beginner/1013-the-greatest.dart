82351c7b0b824c383c734a41f2fdeb30b73f0128
LaksCastro <lakhsmycastro336@gmail.com>
2020-12-28 13:00:32 -0400

    Create 1013-the-greatest.dart

---
+++ dart/beginner/1013-the-greatest.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:math' as math;
+
+void main() {
+  final inputs = _getIntListLine();
+
+  print('${_calcMax(inputs)}');
+}
+
+int _calcMax(List<int> inputs, [int max, int i = 0]) {
+  max = math.max(max ?? inputs[i], inputs[++i]);
+
+  if (i < inputs.length - 1) {
+    return _calcMax(inputs, max, i);
+  }
+
+  return max;
+}
+
+String _getLine() {
+  final line = stdin.readLineSync(encoding: Encoding.getByName('utf-8'));
+
+  return line.trim();
+}
+
+List<int> _getIntListLine() =>
+    _getLine().split(' ').map((o) => int.parse(o)).toList();
