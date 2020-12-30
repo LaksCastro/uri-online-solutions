639c7ac73a28706cefb52d6c1bcdbb56431a5529
LaksCastro <lakhsmycastro336@gmail.com>
2020-12-30 13:13:04 -0400

    Update 1156-s-sequence-ii.dart

--- dart/beginner/1156-s-sequence-ii.dart
+++ dart/beginner/1156-s-sequence-ii.dart
@@ -7,3 +7,14 @@ void main() {
 
   print(s.toStringAsFixed(2));
 }
+
+/// * Recusive solution
+/// ```dart
+///
+/// void main() {
+///   print(_s().toStringAsFixed(2));
+/// }
+///
+/// double _s([double s = 0.0, int d1 = 1, int d2 = 1]) =>
+///   d1 <= 39 ? _s(s += d1 / d2, d1 += 2, d2 += d2) : s;
+/// ```
