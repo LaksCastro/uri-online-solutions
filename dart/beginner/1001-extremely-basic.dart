87df8ad246e0eed5a17a85f577a54a24168264d9
LaksCastro <lakhsmycastro336@gmail.com>
2020-12-30 13:03:28 -0400

    Update 1001-extremely-basic.dart

--- dart/beginner/1001-extremely-basic.dart
+++ dart/beginner/1001-extremely-basic.dart
@@ -5,12 +5,7 @@ void main() {
   print('X = ${_getIntLine() + _getIntLine()}');
 }
 
-String _getLine() {
-  final line = stdin.readLineSync(encoding: Encoding.getByName('utf-8'));
+String _getLine() =>
+    stdin.readLineSync(encoding: Encoding.getByName('utf-8'))?.trim() ?? '';
 
-  return line.trim();
-}
-
-int _getIntLine() {
-  return int.parse(_getLine());
-}
+int _getIntLine() => int.tryParse(_getLine());
