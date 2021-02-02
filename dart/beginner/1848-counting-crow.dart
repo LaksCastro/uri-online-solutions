c3c4c54bcee2d5e9fff38f681bf064ed0fff69e4
Laks <lakhsmycastro336@gmail.com>
2021-02-02 12:22:52 -0300

    Create 1848-counting-crow.dart

---
+++ dart/beginner/1848-counting-crow.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+import 'dart:io';
+
+void main() async {
+  var p = 0, r = 0;
+
+  while (p < 3) {
+    final input = _getLine().replaceAll('*', '1').replaceAll('-', '0');
+
+    if (input == 'caw caw') {
+      ({print(r), r = 0, p++});
+    } else {
+      final value = int.parse(input, radix: 2);
+
+      r += value;
+    }
+  }
+}
+
+String _getLine() =>
+    stdin.readLineSync(encoding: Encoding.getByName('utf-8'))?.trim() ?? '';
