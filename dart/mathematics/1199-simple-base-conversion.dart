58732cc1cc7d5e29c0cf50e1213d37a13e6dfc50
Laks <lakhsmycastro336@gmail.com>
2021-02-07 19:29:13 -0300

    Create 1199-simple-base-conversion.dart

---
+++ dart/mathematics/1199-simple-base-conversion.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'dart:io';
+
+void main() async {
+  String input;
+
+  while ((input = _getLine()) != '-1') {
+    final isHexa = input.startsWith('0x');
+    final n = int.parse(input);
+
+    final prefix = !isHexa ? '0x' : '';
+    final value = n.toRadixString(isHexa ? 10 : 16).toUpperCase();
+
+    print('$prefix$value');
+  }
+}
+
+String _getLine() =>
+    stdin.readLineSync(encoding: Encoding.getByName('utf-8'))?.trim() ?? '';
+
+int _getIntLine() => int.tryParse(_getLine());
+
+List<int> _getIntListLine() => _getLine().split(' ').map(int.tryParse).toList();
