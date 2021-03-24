392e805901d0d705d79dc7294cd91d47ab191a77
Laks <lakhsmycastro336@gmail.com>
2021-03-24 17:40:06 -0300

    Update 1943-top-n.dart

--- dart/ad-hoc/1943-top-n.dart
+++ dart/ad-hoc/1943-top-n.dart
@@ -1,16 +1,16 @@
 import 'dart:convert';
 import 'dart:io';
 
-const tops = [1, 3, 5, 10, 25, 50, 100];
+const _tops = [1, 3, 5, 10, 25, 50, 100];
 
 void main() {
   final n = _getIntLine();
 
   int top;
 
-  for (var i = 0; i < tops.length; i++) {
-    if (n <= tops[i]) {
-      top = tops[i];
+  for (var i = 0; i < _tops.length; i++) {
+    if (n <= _tops[i]) {
+      top = _tops[i];
       break;
     }
   }
