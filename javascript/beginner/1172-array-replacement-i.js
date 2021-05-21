9496e18dd6f4b1a666bb46b78fb14b76e10fc6fd
Laks <lakhsmycastro336@gmail.com>
2021-05-21 01:47:08 -0300

    shortify 1172.js

--- javascript/beginner/1172-array-replacement-i.js
+++ javascript/beginner/1172-array-replacement-i.js
@@ -2,6 +2,5 @@ const input = require("fs").readFileSync("/dev/stdin", "utf8");
 const n = input.split("\n").map(Number);
 
 for (let i = 0; i < 10; i++) {
-  if (n[i] <= 0) n[i] = 1;
-  console.log("X[" + i + "] = " + n[i]);
+  console.log(`X[${i}] = ${n[i] <= 0 ? 1 : n[i]}`);
 }
