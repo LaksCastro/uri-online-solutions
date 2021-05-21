bbb8102bc7eff75324e4c1ecc232a93fefaaf4f7
Laks <lakhsmycastro336@gmail.com>
2021-05-21 01:45:06 -0300

    improve 1172 code, apply https://github.com/LaksCastro/uri-online-solutions/projects/1#card-61288516

--- javascript/beginner/1172-array-replacement-i.js
+++ javascript/beginner/1172-array-replacement-i.js
@@ -1,15 +1,7 @@
-var input = require('fs').readFileSync('/dev/stdin', 'utf8');
-var value = input.split('\n');
+const input = require("fs").readFileSync("/dev/stdin", "utf8");
+const n = input.split("\n").map(Number);
 
-var n = [10]
-
-for (var i = 0; i < 10; i++){
-    n [i] = Number(value.shift());
-}
-for (var i = 0; i < 10; i++){
-    if (n[i] <= 0)
-    n[i] = 1;
-}
-for (var i = 0; i < 10; i++){
-    console.log("X["+ i +"] = " + n[i]);
+for (let i = 0; i < 10; i++) {
+  if (n[i] <= 0) n[i] = 1;
+  console.log("X[" + i + "] = " + n[i]);
 }
